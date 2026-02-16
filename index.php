c863529a1a3b4610fcef26cabadd0798e574e10e
Farshid Mahmoudi <51836304+6arshid@users.noreply.github.com>
2026-02-16 21:20:39 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package Guest Post by Email
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Guest Post by Email
+Plugin URI: http://wordpress.org/plugins/guest-post-wst/
+Description: This is not just a plugin, it's a powerful tool that allows you to receive guest posts via email and publish them on your WordPress site. With this plugin, you can easily manage and publish guest posts without the need for any technical knowledge. Simply set up an email address for receiving guest posts, and the plugin will automatically convert the emails into draft posts in your WordPress dashboard. You can then review, edit, and publish the guest posts with ease. This plugin is perfect for bloggers, content creators, and website owners who want to encourage guest contributions and expand their content offerings.
+Version: 1.0.0
+Author URI: http://whitestudio.team/
+Text Domain: guest-post-wst
+*/
+
+// Do not load directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
